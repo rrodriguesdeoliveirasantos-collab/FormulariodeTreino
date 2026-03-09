0a33e7a38301fad65de6434b64eac519a0a5437e
rrodriguesdeoliveirasantos-collab <rrodriguesdeoliveirasantos@gmail.com>
2026-03-09 20:21:10 -0300

    aplicando css e ajustando identaçao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 let contadorDias = 0
 
-function adicionarDia(){
+function adicionarDia() {
 
-if(contadorDias >= 6){
-alert("Máximo de 6 dias de treino")
-return
-}
+     if (contadorDias >= 6) {
+          alert("Máximo de 6 dias de treino")
+          return
+     }
 
-contadorDias++
+     contadorDias++
 
-const container = document.getElementById("dias-container")
+     const container = document.getElementById("dias-container")
 
-const bloco = document.createElement("div")
-bloco.className = "dia"
+     const bloco = document.createElement("div")
+     bloco.className = "dia"
 
-bloco.innerHTML = `
+     bloco.innerHTML = `
 <h3>Dia ${contadorDias}</h3>
 
 <input class="titulo-dia" placeholder="Ex: Inferiores + Cardio">
@@ -24,173 +24,173 @@ bloco.innerHTML = `
 <input class="observacao" placeholder="Observação (opcional)">
 
 <button class="remover-dia" onclick="removerDia(this)">
-❌ Remover dia</button>
+ Remover dia</button>
 `
 
-container.appendChild(bloco)
+     container.appendChild(bloco)
 
 }
 
-function gerarFicha(){
+function gerarFicha() {
 
-const nome = document.getElementById("nome").value
+     const nome = document.getElementById("nome").value
 
-if (nome.trim() === ""){
-alert("Por favor, preencha o nome do aluno")
-return
-}
+     if (nome.trim() === "") {
+          alert("Por favor, preencha o nome do aluno")
+          return
+     }
 
-const objetivo = document.getElementById("objetivo").value
+     const objetivo = document.getElementById("objetivo").value
 
-if (objetivo.trim() === ""){
-alert("Por favor, preencha o objetivo do aluno")
-return
-}
+     if (objetivo.trim() === "") {
+          alert("Por favor, preencha o objetivo do aluno")
+          return
+     }
 
 
-const dias = document.querySelectorAll(".dia")
+     const dias = document.querySelectorAll(".dia")
 
-if(dias.length === 0){
-alert("Adicione pelo menos um dia de treino")
-return
-}
+     if (dias.length === 0) {
+          alert("Adicione pelo menos um dia de treino")
+          return
+     }
 
-let html = `
+     let html = `
 <h1>Ficha de Treino</h1>
 <p><b>Aluno:</b> ${nome}</p>
 <p><b>Objetivo:</b> ${objetivo}</p>
 `
 
-dias.forEach((dia,index)=>{
+     dias.forEach((dia, index) => {
 
-const titulo = dia.querySelector(".titulo-dia").value
-const exercicios = dia.querySelector(".exercicios").value
-const obs = dia.querySelector(".observacao").value
+          const titulo = dia.querySelector(".titulo-dia").value
+          const exercicios = dia.querySelector(".exercicios").value
+          const obs = dia.querySelector(".observacao").value
 
-const lista = exercicios.split("\n")
-const filtrados = lista.filter(ex => ex.trim() !== "")
-const itens = filtrados.map(ex => `<li>${ex}</li>`)
-const htmlExercicios = `<ul>${itens.join("")}</ul>`
+          const lista = exercicios.split("\n")
+          const filtrados = lista.filter(ex => ex.trim() !== "")
+          const itens = filtrados.map(ex => `<li>${ex}</li>`)
+          const htmlExercicios = `<ul>${itens.join("")}</ul>`
 
-html += `
+          html += `
 <div class="card">
-<h2>Dia ${index+1} - ${titulo}</h2>
+<h2>Dia ${index + 1} - ${titulo}</h2>
 ${htmlExercicios}
 `
 
-if(obs.trim() !== ""){
-html += `<p class="obs">${obs}</p>`
-}
+          if (obs.trim() !== "") {
+               html += `<p class="obs">${obs}</p>`
+          }
 
-html += `</div>`
+          html += `</div>`
 
-})
+     })
 
-document.getElementById("ficha").innerHTML = html
-
-}
-
-function gerarPDF(){
-window.print()
-}
-
-function attNunDias(){
-
-const dias = document.querySelectorAll(".dia")
-dias.forEach((dia,index)=>{
-const titulo = dia.querySelector("h3")
-titulo.textContent = `Dia ${index+1}`
-})
-
-contadorDias = dias.length
+     document.getElementById("ficha").innerHTML = html
 
 }
 
-
-function removerDia(botao){
-
-const confirmar = confirm("Tem certeza que deseja remover este dia?")
-
-if(confirmar){
-botao.parentElement.remove()
-
-attNunDias()
- 
+function gerarPDF() {
+     window.print()
 }
 
-}
+function attNunDias() {
 
-function limparFormulario(){
-    const confirmar = confirm("Tem certeza que deseja limpar o formulário?")
+     const dias = document.querySelectorAll(".dia")
+     dias.forEach((dia, index) => {
+          const titulo = dia.querySelector("h3")
+          titulo.textContent = `Dia ${index + 1}`
+     })
 
-    if(!confirmar) return
-  
-    document.getElementById("nome").value = ""
-    document.getElementById("objetivo").value = ""
-
-    
-    document.getElementById("dias-container").innerHTML = ""
-    
-    
-    document.getElementById("ficha").innerHTML = ""
-
-    contadorDias = 0
-
+     contadorDias = dias.length
 
 }
 
-function salvarDados(){
 
-const nome = document.getElementById("nome").value
-const objetivo = document.getElementById("objetivo").value
+function removerDia(botao) {
 
-localStorage.setItem("nome", nome)
-localStorage.setItem("objetivo", objetivo)
+     const confirmar = confirm("Tem certeza que deseja remover este dia?")
+
+     if (confirmar) {
+          botao.parentElement.remove()
+
+          attNunDias()
+
+     }
+
+}
+
+function limparFormulario() {
+     const confirmar = confirm("Tem certeza que deseja limpar o formulário?")
+
+     if (!confirmar) return
+
+     document.getElementById("nome").value = ""
+     document.getElementById("objetivo").value = ""
+
+
+     document.getElementById("dias-container").innerHTML = ""
+
+
+     document.getElementById("ficha").innerHTML = ""
+
+     contadorDias = 0
+
+
+}
+
+function salvarDados() {
+
+     const nome = document.getElementById("nome").value
+     const objetivo = document.getElementById("objetivo").value
+
+     localStorage.setItem("nome", nome)
+     localStorage.setItem("objetivo", objetivo)
 
 }
 
 
 document
-.getElementById("nome")
-.addEventListener("input", salvarDados)
+     .getElementById("nome")
+     .addEventListener("input", salvarDados)
 
 document
-.getElementById("objetivo")
-.addEventListener("input", salvarDados)
+     .getElementById("objetivo")
+     .addEventListener("input", salvarDados)
 
 
-function carregarDados(){
-const nome = localStorage.getItem("nome")
-const objetivo = localStorage.getItem("objetivo")
+function carregarDados() {
+     const nome = localStorage.getItem("nome")
+     const objetivo = localStorage.getItem("objetivo")
 
-if(nome){
-     document.getElementById("nome").value = nome
+     if (nome) {
+          document.getElementById("nome").value = nome
+     }
+     if (objetivo) {
+          document.getElementById("objetivo").value = objetivo
+     }
+
 }
-if(objetivo){
-     document.getElementById("objetivo").value = objetivo
-}
 
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function(){
+     carregarDados()
 
-carregarDados()
+     document
+          .getElementById("gerarFicha")
+          .addEventListener("click", gerarFicha)
 
-document
-.getElementById("gerarFicha")
-.addEventListener("click", gerarFicha)
+     document
+          .getElementById("adicionarDia")
+          .addEventListener("click", adicionarDia)
 
-document
-.getElementById("adicionarDia")
-.addEventListener("click", adicionarDia)
-
-document
-.getElementById("gerarPDF")
-.addEventListener("click", gerarPDF)
+     document
+          .getElementById("gerarPDF")
+          .addEventListener("click", gerarPDF)
 
 
-document
-.getElementById("limparFormulario")
-.addEventListener("click", limparFormulario)
+     document
+          .getElementById("limparFormulario")
+          .addEventListener("click", limparFormulario)
 
 })
