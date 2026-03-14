e47637630a042b7035059e7eaa2d80d62e8c634e
rrodriguesdeoliveirasantos-collab <rrodriguesdeoliveirasantos@gmail.com>
2026-03-14 05:53:31 -0300

    melhorias e alguns testes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 let contadorDias = 0
 
+const bibliotecaExercicios = ["Agachamento",
+"Leg Press",
+"Cadeira Extensora",
+"Cadeira Flexora",
+"Elevação Pélvica",
+"Afundo",
+"Supino",
+"Remada",
+"Puxada na Frente",
+"Elevação Lateral",
+"Rosca Bíceps",
+"Tríceps Polia",
+"Prancha",
+"Abdominal"]
+
 function adicionarDia() {
 
      if (contadorDias >= 6) {
@@ -19,15 +34,47 @@ function adicionarDia() {
 
 <input class="titulo-dia" placeholder="Ex: Inferiores + Cardio">
 
+<select class="select-exercicio">
+     <option value="">Adicionar exercício</option>
+</select>
+
 <textarea class="exercicios" placeholder="Um exercício por linha"></textarea>
 
 <input class="observacao" placeholder="Observação (opcional)">
 
-<button class="remover-dia" onclick="removerDia(this)">
+<button type="button" class="remover-dia" onclick="removerDia(this)">
  Remover dia</button>
 `
 
      container.appendChild(bloco)
+
+
+     const select = bloco.querySelector(".select-exercicio")
+
+     bibliotecaExercicios.forEach(exercicio => {
+          const option = document.createElement("option")
+
+
+          option.value = exercicio
+          option.textContent = exercicio
+
+
+          select.appendChild(option)
+     })
+
+     select.addEventListener("change", function () {
+
+          if (this.value === "") return
+
+               const textarea = bloco.querySelector(".exercicios")
+               textarea.value += this.value + "\n"
+               this.value = ""          
+
+     })
+     bloco.querySelector(".titulo-dia").addEventListener("input", salvarFicha)
+     bloco.querySelector(".exercicios").addEventListener("input", salvarFicha)
+     bloco.querySelector(".observacao").addEventListener("input", salvarFicha)
+
 
 }
 
@@ -47,7 +94,6 @@ function gerarFicha() {
           return
      }
 
-
      const dias = document.querySelectorAll(".dia")
 
      if (dias.length === 0) {
@@ -63,13 +109,19 @@ function gerarFicha() {
 
      dias.forEach((dia, index) => {
 
-          const titulo = dia.querySelector(".titulo-dia").value
+          let titulo = dia.querySelector(".titulo-dia").value
+
+          if (titulo.trim() === "") {
+               titulo = "Treino"
+          }
+
           const exercicios = dia.querySelector(".exercicios").value
           const obs = dia.querySelector(".observacao").value
 
           const lista = exercicios.split("\n")
           const filtrados = lista.filter(ex => ex.trim() !== "")
           const itens = filtrados.map(ex => `<li>${ex}</li>`)
+
           const htmlExercicios = `<ul>${itens.join("")}</ul>`
 
           html += `
@@ -91,7 +143,10 @@ ${htmlExercicios}
 }
 
 function gerarPDF() {
-     window.print()
+
+     const elemento = document.getElementById("ficha")
+
+     html2pdf().from(elemento).save()
 }
 
 function attNunDias() {
@@ -113,12 +168,13 @@ function removerDia(botao) {
 
      if (confirmar) {
           botao.parentElement.remove()
-
-          attNunDias()
-
+          attNunDias() // Atualiza os títulos dos dias e o contador
      }
 
 }
+
+
+
 
 function limparFormulario() {
      const confirmar = confirm("Tem certeza que deseja limpar o formulário?")
@@ -172,9 +228,151 @@ function carregarDados() {
 
 }
 
+
+function salvarFicha() {
+
+     const nome = document.getElementById("nome").value
+     const objetivo = document.getElementById("objetivo").value
+
+     const dias = document.querySelectorAll(".dia")
+
+     let diasTreino = []
+
+     dias.forEach(dia => {
+
+          const titulo = dia.querySelector(".titulo-dia").value
+          const exercicios = dia.querySelector(".exercicios").value
+          const obs = dia.querySelector(".observacao").value
+
+          diasTreino.push({
+               titulo: titulo,
+               exercicios: exercicios,
+               obs: obs
+          })
+
+     })
+
+     const ficha = {
+          nome: nome,
+          objetivo: objetivo,
+          dias: diasTreino
+     }
+
+     let fichas = JSON.parse(localStorage.getItem("fichas")) || []
+
+     fichas.push(ficha)
+
+     localStorage.setItem("fichas", JSON.stringify(fichas))
+
+     alert("Ficha salva com sucesso!")
+
+     listarFichas()
+
+}
+
+
+function carregarFicha() {
+
+     const dados = localStorage.getItem("diasTreino")
+
+     if (!dados) return
+
+
+     const lista = JSON.parse(dados)
+
+
+     lista.forEach(dia => {
+
+
+          adicionarDia()
+
+          const dias = document.querySelectorAll(".dia")
+          const ultimo = dias[dias.length - 1]
+
+
+          ultimo.querySelector(".titulo-dia").value = dia.titulo
+          ultimo.querySelector(".exercicios").value = dia.exercicios
+          ultimo.querySelector(".observacao").value = dia.obs
+     })
+}
+
+
+function listarFichas() {
+
+     const fichas = JSON.parse(localStorage.getItem("fichas")) || []
+
+     const container = document.getElementById("lista-fichas")
+    
+     container.innerHTML = ""
+
+     fichas.forEach((ficha, index) => {
+    const div = document.createElement("div")
+
+    div.className = "ficha-salva"
+    
+    div.innerHTML = `
+    <b>${ficha.nome}</b> - ${ficha.objetivo}
+    <button type="button" onclick="abrirFicha(${index})">
+    Abrir</button>
+
+    <button type="button" onclick="excluirFicha(${index})">
+    Excluir</button>
+
+    `
+
+    container.appendChild(div)
+ })
+
+}
+
+
+function abrirFicha(index) {
+
+     const fichas = JSON.parse(localStorage.getItem("fichas"))
+
+     const ficha = fichas[index]
+
+     document.getElementById("nome").value = ficha.nome
+     document.getElementById("objetivo").value = ficha.objetivo
+
+     document.getElementById("dias-container").innerHTML = ""
+
+     contadorDias = 0
+
+     ficha.dias.forEach(dia => {
+          adicionarDia()
+          const dias = document.querySelectorAll(".dia")
+          const ultimo = dias[dias.length - 1]
+
+          ultimo.querySelector(".titulo-dia").value = dia.titulo
+          ultimo.querySelector(".exercicios").value = dia.exercicios
+          ultimo.querySelector(".observacao").value = dia.obs
+     })
+
+
+}
+
+
+function excluirFicha(index) {
+
+     const confirmar = confirm("Tem certeza que deseja excluir esta ficha?")
+
+     if (!confirmar) return
+
+     let fichas = JSON.parse(localStorage.getItem("fichas")) || []
+
+     fichas.splice(index, 1)
+
+     localStorage.setItem("fichas", JSON.stringify(fichas))
+
+     listarFichas()
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
      carregarDados()
+     listarFichas()
 
      document
           .getElementById("gerarFicha")
@@ -194,3 +392,5 @@ document.addEventListener("DOMContentLoaded", function () {
           .addEventListener("click", limparFormulario)
 
 })
+document.getElementById("salvarFicha")
+.addEventListener("click", salvarFicha)
